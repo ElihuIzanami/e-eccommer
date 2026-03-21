8dddea5e9e363433798c01d907b03ed356d5fe5a
Izanami49 <elihu.montoya.estrada@gmail.com>
2026-03-20 18:35:34 -0600

    Subiendo el proyecto de e-eccomer

---
+++ src/main/java/com/eime/eccomer/EEccomerApplication.java
@@ -0,0 +1,13 @@
+package com.eime.eccomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EEccomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EEccomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eime/eccomer/EEccomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eime.eccomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EEccomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

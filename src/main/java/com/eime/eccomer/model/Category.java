754c17265f6e0701afe3d14ff7a0beee33d6c4b3
Izanami49 <elihu.montoya.estrada@gmail.com>
2026-06-25 19:38:59 -0600

    Creacion del Modelo Address Role User

--- src/main/java/com/eime/eccomer/model/Category.java
+++ src/main/java/com/eime/eccomer/model/Category.java
@@ -15,16 +15,14 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Category {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
 
     @NotBlank
-    @Size(min=5, message = "Category name must contain atleast 5 characters")
+    @Size(min = 5, message = "Category name must contain atleast 5 characters")
     private String categoryName;
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
     private List<Product> products;
-
 }
